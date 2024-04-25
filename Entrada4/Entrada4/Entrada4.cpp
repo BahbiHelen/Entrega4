d09d74b41605f1e6efbaa54eb21b2890769b61c9
Bahbi <133366088+BahbiHelen@users.noreply.github.com>
2024-04-25 13:16:44 -0300

    Add files via upload

---
+++ Entrada4/Entrada4/Entrada4.cpp
@@ -0,0 +1,26 @@
+using System;
+
+namespace PrimeiroProjeto
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int numero1 = 300;
+            int numero2 = 20;
+
+
+            int divisao = numero1 / numero2;
+
+
+            Console.WriteLine("essa é a quantidade de caixas utilizadas: " + divisao);
+
+
+
+
+
+        }c
+
+    }
